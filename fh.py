00456e4509a7d15ced45176d72d4e2f5a4bbc1bd
Fancc666 <60925834+Fancc666@users.noreply.github.com>
2022-08-25 17:27:16 +0800

    Update fh.py

--- fh.py
+++ fh.py
@@ -1,7 +1,14 @@
-f = open('z8.json', 'r')
-g = open('z81.json', 'w')
-for x in range(12197):
+f = open('mywords.json', 'r')# 改成目前的文件名
+g = open('mywords_.json', 'w')# 改成最终生成并使用的字库文件名
+flen = len(f.readlines())
+f.seek(0, 0)
+g.write("response={'dictionary':[")
+for x in range(flen):
+    if x == flen - 1:
+        g.write(f.readline())
+        continue
     g.write(f.readline()[:-1]+","+"\n")
     # print(x)
+g.write("]}")
 f.close()
 g.close()
